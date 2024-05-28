ed2ebb48730b83caac2b4ffc0038bc71a976375c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-05-28 21:25:46 +0100

    Fix code after changes to dependencies

--- include/Ishiko/IO/IOErrorCategory.hpp
+++ include/Ishiko/IO/IOErrorCategory.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2020-2022 Xavier Leclercq
+    Copyright (c) 2020-2024 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/io/blob/main/LICENSE.txt
 */
@@ -25,7 +25,7 @@ public:
     static const IOErrorCategory& Get() noexcept;
 
     const char* name() const noexcept override;
-    std::ostream& streamOut(int value, std::ostream& os) const override;
+    const char* message(int ev, char* buffer, size_t len) const noexcept override;
 
 private:
     IOErrorCategory() noexcept = default;
